36a9c305748350295350c925fc0f3f2b37f5d26f
VladimirovRoman <vladimirovrm@gmail.com>
2022-11-01 10:01:56 +0300

    Update Result.jsx

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -5,7 +5,7 @@ function Result({ correct, questions }) {
 		<div className='result'>
 			<img src='https://cdn-icons-png.flaticon.com/512/2278/2278992.png' />
 			<h2>
-				Вы отгадали {correct} {correct <= 1 ? 'ответа' : 'ответов'} из{' '}
+				Ваш результат {correct} из {''}
 				{questions.length}
 			</h2>
 			<button onClick={() => window.location.reload()}>
