f96bee4874854d5924e920d13f4942c319c30612
VladimirovRoman <vladimirovrm@gmail.com>
2022-11-03 14:50:51 +0300

    Update Result.jsx
    Изменение отображения результатов

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -5,7 +5,15 @@ function Result({ correct, questions }) {
 		<div className='result'>
 			<img src='https://cdn-icons-png.flaticon.com/512/2278/2278992.png' />
 			<h2>
-				Ваш результат {correct} из {''}
+				Вы отгадали{' '}
+				{correct === 0 || correct > 5
+					? `${correct} ответов` // работает
+					: correct === 1 // работает
+					? `${correct} ответ`
+					: correct === 2 || correct === 3 || correct === 4
+					? `${correct} ответа `
+					: null}{' '}
+				из {''}
 				{questions.length}
 			</h2>
 			<button onClick={() => window.location.reload()}>
